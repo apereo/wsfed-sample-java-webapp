6adfbce8d5abfebd2d1642e31ac0b1cbece997ad
hdeadman <hal.deadman@gmail.com>
2022-02-13 20:54:51 -0500

    fix logging

--- src/main/java/org/apereo/cas/ClassLoaderLeakPreventorListener.java
+++ src/main/java/org/apereo/cas/ClassLoaderLeakPreventorListener.java
@@ -2,6 +2,9 @@ package org.apereo.cas;
 
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Extend ClassLoaderLeakPreventorListener to override standard logger (route it to slf4j) so we can control logging.
  * This might risk classloader leak but we just want threads stopped.
@@ -11,5 +14,6 @@ public class ClassLoaderLeakPreventorListener
     static {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
+        Logger.getLogger("").setLevel(Level.FINEST);
     }
 }
